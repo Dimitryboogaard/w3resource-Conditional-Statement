323e376aef935bae5f9d159ce36ad985091f1ac1
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-04-14 19:47:15 +0200

    Add files via upload

---
+++ 25/25/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace _25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbGetal1 = new System.Windows.Forms.TextBox();
+            this.tbGetal2 = new System.Windows.Forms.TextBox();
+            this.lblGetal1 = new System.Windows.Forms.Label();
+            this.lblGetal2 = new System.Windows.Forms.Label();
+            this.rbOptellen = new System.Windows.Forms.RadioButton();
+            this.rbAftrekken = new System.Windows.Forms.RadioButton();
+            this.rbVermenigVuldigen = new System.Windows.Forms.RadioButton();
+            this.rbDelen = new System.Windows.Forms.RadioButton();
+            this.rbExit = new System.Windows.Forms.RadioButton();
+            this.tbAntwoord = new System.Windows.Forms.TextBox();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.lblAntwoord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbGetal1
+            // 
+            this.tbGetal1.Location = new System.Drawing.Point(70, 12);
+            this.tbGetal1.Name = "tbGetal1";
+            this.tbGetal1.Size = new System.Drawing.Size(100, 20);
+            this.tbGetal1.TabIndex = 0;
+            // 
+            // tbGetal2
+            // 
+            this.tbGetal2.Location = new System.Drawing.Point(70, 38);
+            this.tbGetal2.Name = "tbGetal2";
+            this.tbGetal2.Size = new System.Drawing.Size(100, 20);
+            this.tbGetal2.TabIndex = 1;
+            // 
+            // lblGetal1
+            // 
+            this.lblGetal1.AutoSize = true;
+            this.lblGetal1.Location = new System.Drawing.Point(23, 15);
+            this.lblGetal1.Name = "lblGetal1";
+            this.lblGetal1.Size = new System.Drawing.Size(41, 13);
+            this.lblGetal1.TabIndex = 2;
+            this.lblGetal1.Text = "Getal 1";
+            // 
+            // lblGetal2
+            // 
+            this.lblGetal2.AutoSize = true;
+            this.lblGetal2.Location = new System.Drawing.Point(23, 41);
+            this.lblGetal2.Name = "lblGetal2";
+            this.lblGetal2.Size = new System.Drawing.Size(41, 13);
+            this.lblGetal2.TabIndex = 3;
+            this.lblGetal2.Text = "Getal 2";
+            // 
+            // rbOptellen
+            // 
+            this.rbOptellen.AutoSize = true;
+            this.rbOptellen.Location = new System.Drawing.Point(70, 64);
+            this.rbOptellen.Name = "rbOptellen";
+            this.rbOptellen.Size = new System.Drawing.Size(64, 17);
+            this.rbOptellen.TabIndex = 4;
+            this.rbOptellen.TabStop = true;
+            this.rbOptellen.Text = "Optellen";
+            this.rbOptellen.UseVisualStyleBackColor = true;
+            // 
+            // rbAftrekken
+            // 
+            this.rbAftrekken.AutoSize = true;
+            this.rbAftrekken.Location = new System.Drawing.Point(70, 87);
+            this.rbAftrekken.Name = "rbAftrekken";
+            this.rbAftrekken.Size = new System.Drawing.Size(71, 17);
+            this.rbAftrekken.TabIndex = 5;
+            this.rbAftrekken.TabStop = true;
+            this.rbAftrekken.Text = "Aftrekken";
+            this.rbAftrekken.UseVisualStyleBackColor = true;
+            // 
+            // rbVermenigVuldigen
+            // 
+            this.rbVermenigVuldigen.AutoSize = true;
+            this.rbVermenigVuldigen.Location = new System.Drawing.Point(70, 110);
+            this.rbVermenigVuldigen.Name = "rbVermenigVuldigen";
+            this.rbVermenigVuldigen.Size = new System.Drawing.Size(109, 17);
+            this.rbVermenigVuldigen.TabIndex = 6;
+            this.rbVermenigVuldigen.TabStop = true;
+            this.rbVermenigVuldigen.Text = "Vermenigvuldigen";
+            this.rbVermenigVuldigen.UseVisualStyleBackColor = true;
+            // 
+            // rbDelen
+            // 
+            this.rbDelen.AutoSize = true;
+            this.rbDelen.Location = new System.Drawing.Point(70, 133);
+            this.rbDelen.Name = "rbDelen";
+            this.rbDelen.Size = new System.Drawing.Size(53, 17);
+            this.rbDelen.TabIndex = 7;
+            this.rbDelen.TabStop = true;
+            this.rbDelen.Text = "Delen";
+            this.rbDelen.UseVisualStyleBackColor = true;
+            // 
+            // rbExit
+            // 
+            this.rbExit.AutoSize = true;
+            this.rbExit.Location = new System.Drawing.Point(70, 156);
+            this.rbExit.Name = "rbExit";
+            this.rbExit.Size = new System.Drawing.Size(42, 17);
+            this.rbExit.TabIndex = 8;
+            this.rbExit.TabStop = true;
+            this.rbExit.Text = "Exit";
+            this.rbExit.UseVisualStyleBackColor = true;
+            // 
+            // tbAntwoord
+            // 
+            this.tbAntwoord.Location = new System.Drawing.Point(70, 208);
+            this.tbAntwoord.Name = "tbAntwoord";
+            this.tbAntwoord.Size = new System.Drawing.Size(100, 20);
+            this.tbAntwoord.TabIndex = 9;
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(70, 179);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 10;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAntwoord
+            // 
+            this.lblAntwoord.AutoSize = true;
+            this.lblAntwoord.Location = new System.Drawing.Point(12, 211);
+            this.lblAntwoord.Name = "lblAntwoord";
+            this.lblAntwoord.Size = new System.Drawing.Size(52, 13);
+            this.lblAntwoord.TabIndex = 11;
+            this.lblAntwoord.Text = "Antwoord";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 250);
+            this.Controls.Add(this.lblAntwoord);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.tbAntwoord);
+            this.Controls.Add(this.rbExit);
+            this.Controls.Add(this.rbDelen);
+            this.Controls.Add(this.rbVermenigVuldigen);
+            this.Controls.Add(this.rbAftrekken);
+            this.Controls.Add(this.rbOptellen);
+            this.Controls.Add(this.lblGetal2);
+            this.Controls.Add(this.lblGetal1);
+            this.Controls.Add(this.tbGetal2);
+            this.Controls.Add(this.tbGetal1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbGetal1;
+        private System.Windows.Forms.TextBox tbGetal2;
+        private System.Windows.Forms.Label lblGetal1;
+        private System.Windows.Forms.Label lblGetal2;
+        private System.Windows.Forms.RadioButton rbOptellen;
+        private System.Windows.Forms.RadioButton rbAftrekken;
+        private System.Windows.Forms.RadioButton rbVermenigVuldigen;
+        private System.Windows.Forms.RadioButton rbDelen;
+        private System.Windows.Forms.RadioButton rbExit;
+        private System.Windows.Forms.TextBox tbAntwoord;
+        private System.Windows.Forms.Button btnAntwoord;
+        private System.Windows.Forms.Label lblAntwoord;
+    }
+}
+
---
+++ 25/25/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int intGetal1 = Convert.ToInt32(tbGetal1.Text);
+            int intGetal2 = Convert.ToInt32(tbGetal2.Text);
+
+            if(rbOptellen.Checked)
+            {
+                tbAntwoord.Text = Convert.ToString(intGetal1 + intGetal2);
+            }
+           
+            if(rbAftrekken.Checked)
+            {
+                tbAntwoord.Text = Convert.ToString(intGetal1 - intGetal2);
+            }
+
+            if(rbVermenigVuldigen.Checked)
+            {
+                tbAntwoord.Text = Convert.ToString(intGetal1 * intGetal2);
+
+            }
+
+            if(rbDelen.Checked)
+            {
+                tbAntwoord.Text = Convert.ToString(intGetal1 / intGetal2);
+
+            }
+
+            if(rbExit.Checked)
+            {
+                tbGetal1.Text = "";
+                tbGetal2.Text = "";
+            }
+        }
+    }
+}
