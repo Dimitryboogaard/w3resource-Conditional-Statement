54a942bece62732af32db41ecbf7f5ddf5b4f346
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-04-14 19:32:58 +0200

    Add files via upload

---
+++ 1/1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToInt16(tbGetal1.Text) == Convert.ToInt16(tbGetal2.Text))
+            {
+                lblAntwoord.Text = tbGetal1.Text + " en " + tbGetal2.Text + " zijn gelijk.";
+            }
+        }
+    }
+}
---
+++ 3/3/3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToInt16(tbInvoer.Text) < 0)
+            {
+                lblAntwoord.Text = tbInvoer.Text + " is negatief.";
+            }
+
+            else
+            {
+                lblAntwoord.Text = tbInvoer.Text + " is positief.";
+
+            }
+        }
+    }
+}
